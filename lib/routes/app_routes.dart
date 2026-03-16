f1ba27a21f1f107689ef3d9d43bef9efa2abbc95
Hongsophaline <hong.sophaline@institute.pse.ngo>
2026-03-16 15:24:11 +0700

    add detail property

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,7 +1,7 @@
 import 'package:property/screen/login.dart';
 import 'package:property/screen/profile.dart';
 import 'package:property/screen/signup.dart';
-
+import 'package:property/screen/detailproperty.dart';
 import '../screen/home.dart';
 
 class AppRoutes {
@@ -9,11 +9,13 @@ class AppRoutes {
   static const login = '/login';
   static const signup = '/signup';
   static const profile = '/profile';
+  static const detailproperty = '/detailproperty';
 
   static final routes = {
     home: (context) => MyHomePage(title: 'Purple Life Home'),
     login: (context) => MyApp(),
     signup: (context) => SignupPage(),
     profile: (context) => ProfilePage(),
+    detailproperty: (context) => PropertyDetailPage(),
   };
 }
