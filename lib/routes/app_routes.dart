9c2645848cf2f8d98649fc6e88c9136c35ae3d5a
Mary Thoeun <mary@example.com>
2026-03-17 16:24:24 +0700

    add home page and navbar

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,27 +1,31 @@
-import 'package:property/screen/login.dart';
-import 'package:property/screen/profile.dart';
-import 'package:property/screen/signup.dart';
-import 'package:property/screen/detailproperty.dart';
-import 'package:property/screen/edit_profile.dart';
-import 'package:property/screen/favorite.dart';
-
-import '../screen/home.dart';
+import 'package:flutter/material.dart';
+import 'package:property_g3/screen/login.dart';
+import 'package:property_g3/screen/profile.dart';
+import 'package:property_g3/screen/signup.dart';
+import 'package:property_g3/screen/detailproperty.dart';
+import 'package:property_g3/screen/edit_profile.dart';
+import 'package:property_g3/screen/favorite.dart';
+import 'package:property_g3/screen/navbar.dart'; 
+import 'package:property_g3/screen/home.dart';
 
 class AppRoutes {
-  static const home = '/';
-  static const login = '/login';
-  static const signup = '/signup';
-  static const profile = '/profile';
-  static const detailproperty = '/detailproperty';
-  static const editProfile = '/edit-profile';
-  static const favorite = '/favorite';
-  static final routes = {
-    home: (context) => MyHomePage(title: 'Purple Life Home'),
-    login: (context) => MyApp(),
-    signup: (context) => SignupPage(),
-    profile: (context) => ProfilePage(),
-    favorite: (context) => FavoritePage(),
-    detailproperty: (context) => PropertyDetailPage(),
+  static const String navbar = '/navbar';
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String profile = '/profile';
+  static const String detailproperty = '/detailproperty';
+  static const String editProfile = '/edit-profile';
+  static const String favorite = '/favorite';
+
+  static Map<String, WidgetBuilder> routes = {
+    navbar: (context) => const NavPage(), 
+    home: (context) => const Home(), 
+    login: (context) => const LoginPage(), 
+    signup: (context) => const SignupPage(),
+    profile: (context) => const ProfilePage(),
+    favorite: (context) => const FavoritePage(),
+    detailproperty: (context) => const PropertyDetailPage(),
     editProfile: (context) => const EditProfilePage(),
   };
 }
